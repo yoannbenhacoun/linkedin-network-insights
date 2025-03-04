16a900408cbfaa8cfe62fa3f458361af4fb7f6e4
yoannbenhacoun <175301534+yoannbenhacoun@users.noreply.github.com>
2025-03-04 16:06:10 +0100

    Create route.js

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import LinkedInProvider from "next-auth/providers/linkedin";
+
+const handler = NextAuth({
+  providers: [
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+      authorization: {
+        params: { scope: "openid profile email" },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
